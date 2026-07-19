ab85d6627e6da3b1ba1913a97515fd4bec438313
Ebubekir KARAMUSTAFA <77796622+ekaramustafa@users.noreply.github.com>
2026-07-19 16:04:44 +0300

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.go
@@ -0,0 +1,29 @@
+func characterReplacement(s string, k int) int {
+
+    l := 0
+    r := 0
+    result := 1
+    counter := k;
+    for r < len(s){
+        if s[r] != s[l]{
+            if counter > 0{
+                counter--
+            } else {
+                counter = k
+                for s[r] != s[l] {
+                    l++
+                }
+            }
+            
+        }
+        curr := r - l + 1
+        if curr > result{
+            result = curr
+        }
+        r++
+    }
+
+    return result
+
+
+}
