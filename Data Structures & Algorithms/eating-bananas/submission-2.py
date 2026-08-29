414726693e6219f0a005e6482b5c93777f67e948
Ebubekir KARAMUSTAFA <77796622+ekaramustafa@users.noreply.github.com>
2026-08-30 00:09:59 +0300

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # k = 1
+        # 1 4 3 2
+        # 
+        if h == len(piles):
+            return max(piles)
+        
+        l = 1
+        r = max(piles) # 4
+
+        while l < r:
+            mid = l + (r-l) // 2
+            if self.canSolve(mid, piles.copy(), h):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
+    
+    def canSolve(self, mid, arr, h):
+        ls = [(e + mid - 1) // mid for e in arr]
+        if sum(ls) > h:
+            return False
+        return True
+        
